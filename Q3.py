86502c01551328e70bf8b8d1da6753df821dfa7f
Angel T John <98639433+ATJTECH@users.noreply.github.com>
2023-10-12 23:04:59 +0530

    Add files via upload

--- Q3.py
+++ Q3.py
@@ -1,24 +1,10 @@
-def count_vowels_and_consonants(input_string):
-    vowels = "aeiouAEIOU"
-    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
-    
-    vowel_count = 0
-    consonant_count = 0
-    
-    for char in input_string:
-        if char in vowels:
-            vowel_count += 1
-        elif char in consonants:
-            consonant_count += 1
-    
-    return vowel_count, consonant_count
+#Write a regex pattern to extract the domain
+#(e.g., "example.com") from a URL like "https://www.example.com/page&quot;.
 
-# Get user input
-input_string = input("Enter a string: ")
+import re
 
-# Call the function to count vowels and consonants
-vowel_count, consonant_count = count_vowels_and_consonants(input_string)
+url=input("Enter the url eg:https://www.example.com/page&quot;")
+pattern = r"https?://(www\.)?([a-zA-Z0-9.-]+)"
+domain = re.search(pattern, url).group(2)
 
-# Print the results
-print(f"Number of vowels: {vowel_count}")
-print(f"Number of consonants: {consonant_count}")
+print(domain)
